3aaca7fe45fdb891e4de374753a411a7623054f1
Ira <you@example.com>
2017-10-04 19:00:41 +0300

    create task

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Ближайшие бары
-
-[FIXME. Здесь будет описание проекта]
+Программа выводит самый большой бар и самый маленький бар,
+затем запрашивает данные о координатах пользователя и выводит самый близкий к нему бар.
+Данные о барах берутся из файла bars.json
 
 # Как запустить
 
@@ -11,8 +12,15 @@
 ```#!bash
 
 $ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+# Пример ответа
 
+```
+Biggest bar is Спорт бар «Красная машина» which has 450 seats
+Smallest bar is БАР. СОКИ which has 0 seats
+If you want to find closest bar, input your coordinates
+input longitude: 37.383
+input latitude: 55.844
+Closest bar is Соки
 ```
 
 Запуск на Windows происходит аналогично.
--- bars.py
+++ bars.py
@@ -1,21 +1,38 @@
 import json
+import math
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as bars_info:
+        return json.loads(bars_info.read())
 
 
 def get_biggest_bar(data):
-    pass
+    return max([(d["properties"]["Attributes"]["SeatsCount"], d["properties"]["Attributes"]["Name"]) for d in data],
+               key=lambda p: p[0])
 
 
 def get_smallest_bar(data):
-    pass
+    return min([(d["properties"]["Attributes"]["SeatsCount"], d["properties"]["Attributes"]["Name"]) for d in data],
+               key=lambda p: p[0])
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    coords = [(d["geometry"]["coordinates"], d["properties"]["Attributes"]["Name"]) for d in data]
+    distances = []
+    for coord in coords:
+        distance = math.sqrt((float(coord[0][0]) - longitude) ** 2 + (float(coord[0][1]) - latitude) ** 2)
+        distances.append((distance, coord[1]))
+    return min(distances)[1]
 
 
 if __name__ == '__main__':
-    pass
+    bars_list = load_data('bars.json')["features"]
+    biggest_bar_info = get_biggest_bar(bars_list)
+    print('Biggest bar is {} which has {} seats'.format(biggest_bar_info[1], biggest_bar_info[0]))
+    smallest_bar_info = get_smallest_bar(bars_list)
+    print('Smallest bar is {} which has {} seats'.format(smallest_bar_info[1], smallest_bar_info[0]))
+    print('If you want to find closest bar, input your coordinates')
+    longitude = float(input('input longitude: '))
+    latitude = float(input('input latitude: '))
+    print('Closest bar is {}'.format(get_closest_bar(bars_list, longitude, latitude)))
